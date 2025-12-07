38597d2ed4df43d234d0d15de1a00e600baea832
saneness <saneness@gmail.com>
2025-12-07 11:25:54 +0000

    day 07

---
+++ 07.py
@@ -0,0 +1,38 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2025, day=7)
+data = [list(item) for item in puzzle.input_data.split('\n')]
+
+answer_a = 0
+answer_b = 0
+
+start = data[0].index('S')
+data[1][start] = 1
+
+for i in range(2, len(data)):
+    if i % 2 == 0:
+        for j in range(len(data[i])):
+            if isinstance(data[i-1][j], int):
+                if data[i][j] == '^':
+                    if isinstance(data[i][j-1], int):
+                        data[i][j-1] += data[i-1][j]
+                    else:
+                        data[i][j-1] = data[i-1][j]
+                    if isinstance(data[i][j+1], int):
+                        data[i][j+1] += data[i-1][j]
+                    else:
+                        data[i][j+1] = data[i-1][j]
+                    data[i+1][j-1] = data[i][j-1]
+                    data[i+1][j+1] = data[i][j+1]
+                    answer_a += 1
+                else:
+                    if isinstance(data[i][j], int):
+                        data[i][j] += data[i-1][j]
+                    else:
+                        data[i][j] = data[i-1][j]
+                    data[i+1][j] = data[i][j]
+
+answer_b = sum([item for item in data[-1] if isinstance(item, int)])
+
+puzzle.answer_a = answer_a
+puzzle.answer_b = answer_b
